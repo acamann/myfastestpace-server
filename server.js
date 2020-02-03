b862e399906f322f6a8f99d8aac173ca4c188637
Andy Camann <acamann@gmail.com>
2020-02-03 17:29:16 -0600

    added jwt

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const authConfig = require("./auth_config.json");
+
+// create JWT middleware
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+    }),
+
+    audience: authConfig.audience,
+    issuer: `https://${authConfig.domain}/`,
+    algorithm: ["RS256"]
+});
+
 require('dotenv').config();
 
 const app = express();
@@ -19,6 +37,22 @@ connection.on('error', console.error.bind(console, 'connection error:'));
 connection.once('open', () => {
     console.log("MongoDB database connection established succesfully");
 })
+
+// add protected endpoint for auth0 to prevent unauthorized requests
+app.get("/api/external", checkJwt, (req, res) => {
+    res.send({
+        msg: "Your access token was successfully validated!"
+    });
+});
+
+// Error handler for Auth0
+app.use(function(err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+      return res.status(401).send({ msg: "Invalid token" });
+    }
+  
+    next(err, req, res);
+});
 
 const racesRouter = require('./routes/races');
 const usersRouter = require('./routes/users');
